944c04fd79f2f98ac5ff95a52d07f70815624ed8
erikgm02 <erikgm02@hotmail.com>
2025-10-29 19:42:11 -0600

    Update HiloEscribe.java
    Agregamos una bandera para que nuestro bhilo se pueda pausar, tambien se le agrego dicha bandera para que se peuda reanudar

--- HiloEscribe.java
+++ HiloEscribe.java
@@ -1,43 +1,73 @@
-public class HiloEscribe extends Thread{
+public class HiloEscribe extends Thread {
     private String info[];
     private int ini;
     private int tmp;
     private Posicion p;
 
-    public HiloEscribe(Posicion x){
-        this.p=x;
-    }
     
+    private volatile boolean pausado = false;
+
+    private final Object candadoPausa = new Object();
+
+    public HiloEscribe(Posicion x) {
+        this.p = x;
+    }
+
     public void setTmp(int tmp) {
         this.tmp = tmp;
     }
+
     @Override
-    public void run(){
-        for(int i=ini;i<ini+5;i++)
-        {
-           try{
-                synchronized(p){
-                    info[p.getP()]=getName()+i+"-"+p.getP();
+    public void run() {
+        for (int i = ini; i < ini + 5; i++) {
+            try {
+                
+
+                synchronized (candadoPausa) {
+                    while (pausado) {
+
+                        candadoPausa.wait();
+                    }
+                }
+
+                synchronized (p) {
+                    info[p.getP()] = getName() + i + "-" + p.getP();
                     p.incrementa();
                 }
-                sleep(tmp);
-           }
-            catch(ArrayIndexOutOfBoundsException e){
+                sleep(tmp); 
+
+            } catch (ArrayIndexOutOfBoundsException e) {
                 System.out.println("NO HAY ESPACIO");
-                i=ini+5;
-            }
-           catch(InterruptedException e){
-                System.out.println("INTERRUPCIÓN");
+                i = ini + 5;
+            } catch (InterruptedException e) {
+              
+                System.out.println("Hilo " + getName() + " interrumpido y terminado.");
+                i = ini + 5; 
             }
         }
+        System.out.println("Hilo " + getName() + " ha finalizado su trabajo.");
     }
-    
-    public void setIni(int x){
-        ini=x;
+
+    public void setIni(int x) {
+        ini = x;
     }
-    
-    public void setInfo(String x[]){
-        info=x;
+
+    public void setInfo(String x[]) {
+        info = x;
     }
+
+   
+    public void pausar() {
+        pausado = true;
+    }
+
+    public void reanudar() {
+        pausado = false;
+ 
+        synchronized (candadoPausa) {
+            candadoPausa.notify();
+        }
+    }
+
     
 }
